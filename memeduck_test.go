b97c53fcc154dbcd231962f632885ceb82eeda72
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-05-22 13:40:09 +0900

    Test: split tests

--- memeduck_test.go
+++ memeduck_test.go
@@ -6,47 +6,47 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInsert(t *testing.T) {
-	test := func(is *InsertStmt, expected string) {
-		actual, err := is.SQL()
-		assert.Nil(t, err, expected)
-		assert.Equal(t, expected, actual)
-	}
+func testInsert(t *testing.T, stmt *InsertStmt, expected string) {
+	actual, err := stmt.SQL()
+	assert.Nil(t, err, expected)
+	assert.Equal(t, expected, actual)
+}
 
-	func() {
-		test(
-			Insert("person", []string{"age", "height"}, [][]int{
-				{1600, 143},
-			}),
-			"INSERT INTO person (age, height) VALUES (1600, 143)",
-		)
-	}()
-	func() {
-		var age int = 1600
-		var height int = 143
-		test(
-			Insert("person", []string{"age", "height"}, [][]*int{
-				{&age, &height},
-			}),
-			"INSERT INTO person (age, height) VALUES (1600, 143)",
-		)
-	}()
-	func() {
-		test(
-			Insert("person", []string{"age", "height"}, [][]int64{
-				{1600, 143},
-			}),
-			"INSERT INTO person (age, height) VALUES (1600, 143)",
-		)
-	}()
-	func() {
-		var age int64 = 1600
-		var height int64 = 143
-		test(
-			Insert("person", []string{"age", "height"}, [][]*int64{
-				{&age, &height},
-			}),
-			"INSERT INTO person (age, height) VALUES (1600, 143)",
-		)
-	}()
+func TestInsertWithIntSlice(t *testing.T) {
+	testInsert(t,
+		Insert("person", []string{"age", "height"}, [][]int{
+			{1600, 143},
+		}),
+		"INSERT INTO person (age, height) VALUES (1600, 143)",
+	)
+}
+
+func TestInsertWithIntPtrSlice(t *testing.T) {
+	var age int = 1600
+	var height int = 143
+	testInsert(t,
+		Insert("person", []string{"age", "height"}, [][]*int{
+			{&age, &height},
+		}),
+		"INSERT INTO person (age, height) VALUES (1600, 143)",
+	)
+}
+
+func TestInsertWithInt64Slice(t *testing.T) {
+	testInsert(t,
+		Insert("person", []string{"age", "height"}, [][]int64{
+			{1600, 143},
+		}),
+		"INSERT INTO person (age, height) VALUES (1600, 143)",
+	)
+}
+func TsetInsertWithInt64PtrSlice(t *testing.T) {
+	var age int64 = 1600
+	var height int64 = 143
+	testInsert(t,
+		Insert("person", []string{"age", "height"}, [][]*int64{
+			{&age, &height},
+		}),
+		"INSERT INTO person (age, height) VALUES (1600, 143)",
+	)
 }
